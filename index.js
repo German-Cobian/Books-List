cc34c982d40c53befabb8d56bcd0cc355fcc797e
Zulma Rocio <zrmartinezg@gmail.com>
2021-09-13 15:28:02 -0500

    Add book to Array functionallity

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
+;
 
 const generateId = () => (Math.random() + 1);
+const firstBook = new Book('Crime and Punishment', 'Dostoyesky');
+const secondBook = new Book('Dawn', 'Nieszche');
+let bookArray = [firstBook, secondBook]
 
 function Book(title, author, id) {
   this.title = title;
   this.author = author;
-  this.id = generateId();
+  this.id = generateId();   
+
 }
 
-const firstBook = new Book('Crime and Punishment', 'Dostoyesky');
-const secondBook = new Book('Dawn', 'Nieszche');
-
-let bookArray = [firstBook, secondBook];
-
-const bookCollection = document.querySelector('#collection');
-
-const createBookElement = (book) => {
-  const bookContainer = document.querySelector('#book-collection');
-  bookContainer.id = `${book.id}`;
-  bookContainer.innerHTML = `
-        <p>${book.title}</p>
-        <p>${book.author}</p>
-        <button type="submit" data-action="delete" data-book-id="${book.id}>Remove</button>
-        `;
-  return bookContainer;
+Book.prototype = {
+  ...Book.prototype,
+  addBookToArray() {
+    bookArray.push(this)
+  }
 }
 
+let form = document.querySelector("#book-form");
+
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+  let author = form.elements.author.value;
+  let title = form.elements.title.value;
+  let newBook = new Book (title, author);
+  newBook.addBookToArray();
+})
 
 
 
