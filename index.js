e13ae197c184ec901281dd17477ca3fe85fe3260
German Renato Cobian <germancobian@hotmail.com>
2021-09-14 08:10:25 -0700

    Add display functionalities

--- index.js
+++ index.js
@@ -6,6 +6,40 @@ function Book(title, author, isbn) {
   this.isbn = isbn;
 }
 
+// Display Constructor
+function Display() {}
+
+// Add Book To List
+Display.prototype.addBookToList = function(book){
+  const list = document.getElementById('book-list');
+  // Create tr element
+  const bookItem = document.createElement('div');
+  bookItem.id = `${book.id}`;
+  // Insert columns
+  bookItem.innerHTML = `
+    <p>${book.title}</p>
+    <p>${book.author}</p>
+    <p>${book.isbn}</p>
+    <button><a href="#" class="delete">Remove</a></button>
+  `;
+  list.appendChild(bookItem);
+}
+
+// Delete Book
+Display.prototype.deleteBook = function(target){
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
+// Clear Fields
+Display.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+}
+
+
 // Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
   // Get form values
