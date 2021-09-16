e9e1808e64d7b5f3d75746ee983b8c31f0df9793
Zulma Rocio <zrmartinezg@gmail.com>
2021-09-16 12:16:44 -0500

    Add onclick methods for the nav elements

--- index.js
+++ index.js
@@ -118,3 +118,34 @@ document.getElementById('book-list').addEventListener('click', (e) => {
 
   e.preventDefault();
 });
+
+// addEventListener for the nav
+const listSection = document.getElementById('list');
+const addBookSection = document.getElementById('add-book');
+const contactSection = document.getElementById('contact');
+
+const listNav = document.querySelector('.list');
+const addBookNav = document.querySelector('.add-book');
+const contactNav = document.querySelector('.contact');
+
+listSection.style.display = "none";
+addBookSection.style.display = "block";
+contactSection.style.display = "none";
+
+listNav.onclick = function() {
+ listSection.style.display = "block";
+ addBookSection.style.display = "none";
+ contactSection.style.display = "none";
+} 
+
+addBookNav.onclick = function() {
+ listSection.style.display = "none";
+ addBookSection.style.display = "block";
+ contactSection.style.display = "none";
+} 
+
+contactNav.onclick = function() {
+ listSection.style.display = "none";
+ addBookSection.style.display = "none";
+ contactSection.style.display = "block";
+}
